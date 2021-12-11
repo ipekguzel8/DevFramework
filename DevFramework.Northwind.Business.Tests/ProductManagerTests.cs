c1baf140c23748756fc5be3d672b7680ac71cd40
ipekguzel8 <ipekgzel8@gmail.com>
2021-12-11 15:25:38 +0300

    Proje dosyası ekle.

---
+++ DevFramework.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using DevFramework.Core.DataAccess;
+using DevFramework.Northwind.Business.Concrete.Managers;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace DevFramework.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        //[ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>();
+            Mock<IQueryableRepository<Product>> mock2 = new Mock<IQueryableRepository<Product>>();
+            ProductManager productManager = new ProductManager(mock.Object,mock2.Object);
+            productManager.Add(new Product());
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,62 @@
+﻿
+using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Aspect.Postsharp;
+using DevFramework.Core.DataAccess;
+using System.Transactions;
+using DevFramework.Core.Aspect.Postsharp.ValidationAspects;
+using DevFramework.Core.Aspect.Postsharp.TransactionAspects;
+using DevFramework.Core.Aspect.Postsharp.CacheAspect;
+using DevFramework.Core.CrossCuttingConcerns.Caching.Microsoft;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        public IProductDal _productDal;
+        private readonly IQueryableRepository<Product> _queryable;
+        public ProductManager(IProductDal productDal, IQueryableRepository<Product> queryable)
+        {
+            _queryable = queryable;
+            _productDal = productDal ;
+        }
+        //[FluentValidationAspect(typeof(ProductValidator))]
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+        [CacheAspect(typeof(MemoryCacheManager))]
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p=>p.ProductId==id);
+        }
+        [FluentValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        public Product Update(Product product)
+        {
+            ValidatorTool.FluentValidate(new ProductValidator(), product);
+            return _productDal.Update(product);
+        }
+        [TransactionScopeAspect]
+        public void TransactionalOperation(Product product1, Product product2)
+        {   
+                    _productDal.Add(product1);
+                    _productDal.Update(product2);
+            
+        }
+    }
+}
